2b679e194a8d604645ed40147ce77d677ab01444
Ben Weidenhofer <benweidenhofer@live.com>
2017-03-20 18:56:29 +1030

    Updated half-life.php code

--- display/code/half-life.php
+++ display/code/half-life.php
@@ -59,7 +59,7 @@ function getFloat32($fourchars) {
     $exponent = bindec(substr($bin, 1, 8));
     $exponent = ($exponent)? $exponent - 127 : $exponent;
 
-    if(exponent) {
+    if($exponent) {
         $float = bindec('1'.substr($bin, 9, $exponent));
         $dec = bindec(substr($bin, 9 + $exponent));
         $time = "$float.$dec";
@@ -90,7 +90,7 @@ class serverStatus {
         $this->_socket = fsockopen("udp://" . $this->_ip, $this->_port, $errno, $errstr, 3);
         socket_set_timeout($this->_socket, 1, 0);
 
-        if($tmp = $this->_sockstate()) {
+        if($tmp = $this->_sockState()) {
             // echo $tmp;
             if(!$this->_socket) {
                 echo "Error #" . $errno . ": " . $errstr;
@@ -112,10 +112,10 @@ class serverStatus {
     // Obtains a ping value to the server
     function _ping() {
         debug("[Getting Ping");
-        if($tmp = $this->_sockstate()) {
+        if($tmp = $this->_sockState()) {
             // echo $tmp;
             debug("[Error in socket]");
-            $this->seterror(ERROR_INSOCKET);
+            $this->setError(ERROR_INSOCKET);
             return -1; // Error in socket
         } else {
             $tmp = "";
@@ -123,11 +123,11 @@ class serverStatus {
             $this->_send("ping".chr(0)); // Todo: original code was $this->_send("ÿÿÿÿping".chr(0)); ... was this an encoding error? Check.
 
             while(strlen($tmp) < 4 && (microtime_float()*1000 - $start) < 1000) {
-                $tmp = $this->_getmore();
+                $tmp = $this->_getMore();
             }
 
             if(strlen($tmp) >=4 && substr($tmp, 4, 1) == 'j') {
-                $end = getmicrotime()*1000;
+                $end = microtime_float()*1000;
                 if($end < $start) {
                     echo $end . '\n' . $start;
                     return ($end - $start); // ($end - $start >= 0 ? ($end - $start) : -1; // Will be numeric ping
@@ -144,13 +144,13 @@ class serverStatus {
     // Populates details array
     function getDetails() {
         debug("[Getting details]");
-        if($tmp = $this->_sockstate()) {
+        if($tmp = $this->_sockState()) {
             // echo $tmp;
             $this->setError(ERROR_INSOCKET);
             return -1;
         } else {
-            $this->_send("details".chr(0));
-            $buffer = $this->_getmore();
+            $this->_send("details".chr(0)); // Todo: original code was $this->_send("ÿÿÿÿdetails".chr(0)); ... was this an encoding error? Check.
+            $buffer = $this->_getMore();
             /**
              *  echo $buffer;
              *  for($i = 0; $i < strlen($buffer); i++) {
@@ -246,8 +246,229 @@ class serverStatus {
     // Sets players array
     function getPlayers() {
         debug("[Getting players]");
-        //
+        // $fp = fsockopen("udp://" . $this->_ip, $this->_port);
+        if($tmp = $this->_sockState()) {
+            // echo $tmp;
+            $this->setError(ERROR_INSOCKET);
+            return -1;
+        } else {
+            $this->_send("players".chr(0)); // Todo: original code was $this->_send("ÿÿÿÿplayers".chr(0)); ... was this an encoding error? Check.
+            $buffer = $this->_getMore();
+            $buffer = substr($buffer, 5);
+            $count = ord(substr($buffer, 0, 1)); // Number of active players
+            debug("[A total of " . $count . " active players were discovered]");
+            $buffer = substr($buffer, 1);
+            $tfrags = "";
+            $ttime = 0;
+            $arr = array();
+
+            for($i = 0; i < $count; $i++) {
+                $rfrags = 0.0;
+                $rtime = 0;
+                $stime = 0;
+                $tind = ord(substr($buffer, 0, 1));
+                $buffer = substr($buffer, 1);
+                $tname = "";
+
+                do {
+                    $tmp = substr($buffer, 1);
+                    $buffer = substr($buffer, 1);
+                    if(ord($tmp !=0)) {
+                        $tname .= $tmp;
+                    }
+                } while(ord($tmp != 0));
+
+                $tfrags = substr($buffer, 0, 4);
+                $buffer = substr($buffer, 4);
+                $rtime = getFloat32($tmp);
+                $arr[$i] = array("Index" => $tind, "Name" => $tname, "Frags" => $rfrags, "Time" => $rtime;
+            }
+        }
+        $this->_players = $arr;
+        return 0;
+    }
+
+    function getRules() {
+        debug("[Getting rules]");
+        $multi = 0;
+        // $cvars = array();
+        if($tmp = $this->_sockState()) {
+            $this->setError(ERROR_INSOCKET);
+            return -1;
+        }
+
+        $this->_send("rules".chr(0)); // Todo: original code was $this->_send("ÿÿÿÿrules".chr(0)); ... was this an encoding error? Check.
+        $buffer = $this->_getMore();
+
+        if(strlen($buffer) == 0) {
+            $buffer = $this->_getMore();
+        }
+
+        $tmp = substr($buffer, 0, 5);
+        $buffer = substr($buffer, 5);
+
+        if(substr($tmp, 0, 4) == chr(254).chr(255).chr(255).chr(255)) {
+            // Now, 5 more bytes to look at
+            $multi = 1;
+            for($i = 0; $i < 4; $i++) {
+                $tmp = substr($buffer, 0, 1);
+                $buffer = substr(($buffer, 1);
+            }
+            $tmp = substr($buffer, 0, 5); // ÿÿÿÿE = Rules reponse
+            $buffer = substr($buffer, 5);
+        }
+        $count = ord(substr($buffer, 0, 1));
+        $buffer = substr($buffer, 2); // Number of rules
+        $i = 0;
+        $svar = "";
+
+        while($i < $count) {
+            if(strlen($buffer) == 0 && $multi == 1) {
+                $buffer = $this->_getMore();
+                $tmp = substr($buffer, 0, 5) // pÿÿÿ_
+                $buffer = substr($buffer, 5);
+                $buffer = substr($buffer, 4);
+            }
+            $tmp = substr($buffer, 0, 1);
+            $buffer = substr($buffer, 1);
+            if(ord($tmp) == 0) {
+                $i += 0.5;
+                $svar = $svar.$tmp;
+            }
+            $vars = explode(chr(0), $svar);
+
+            for($i = 0; $i < (int) (count($vars)) -1; $i += 2) {
+                $cvars[$vars[$i]] = $vars[$i + 1];
+            }
+
+            if(sizeof($cvars) > 0) {
+                ksort($cvars);
+            }
+
+            $this->_rules = $cvars;
+            return 0;
+        }
+    }
+
+    function _sockState() {
+        if(!$this->_socket) {
+            return 8;
+        }
+        $status = socket_get_status($this->_socket);
+        $ret = 0;
+
+        // TODO: change this to a switch method
+
+        if($status["timed out"]) {
+            // echo "Error: socket timed out.<br>\n";
+            $ret |= 1;
+        }
+        if($status["eof"]) {
+            // echo "Error: Socket was closed by the remote host.<br>\n";
+            $ret |= 2;
+        }
+        if($status["blocked"]) {
+            /* echo "Error: Port blocked.";
+             * exit;
+             * $ret |= 4;
+             */
+        }
+        return $ret;
+        //return (!$stat["timed_out"] && !$stat["eof"] && !(!$this->_socket));
+    }
+
+    function _send($outstr) {
+        if(!$this->_sockState()) {
+            fwrite($this->_socket, $outstr, strlen($outstr));
+        } else {
+            return "\0";
+        }
+    }
+
+    function _getMore() {
+        if(!$this->_sockState()) {
+            $tmp = fread($this->_socket, 1);
+            $status = socket_get_status($this->_socket);
+
+            if($status["unread_bytes"] == 0) {
+                $status["unread_bytes"] = 1;
+            }
+
+            $tmp .= fread($this->_socket, $status["unread_bytes"]);
+            return $tmp;
+        } else {
+            return "\0";
+        }
+    }
+
+    function _brandSeed() {
+        $this->_seed="\x0a\x3c\x21\x2d\x2d\x20\x20\x20\x20\x20\x20\x20\x53\x65\x72\x76\x65\x72\x20\x6d\x61\x64\x51\x75\x65\x72\x79\x20\x43"
+            ."\x6c\x61\x73\x73\x20\x20\x20\x20\x20\x20\x20\x2d\x2d\x3e\x0a\x3c\x21\x2d\x2d\x20\x20\x20\x20\x43\x6f\x70\x79\x72\x69"
+            ."\x67\x68\x74\x20\x28\x43\x29\x20\x32\x30\x30\x32\x20\x6d\x61\x64\x43\x6f\x64\x65\x72\x20\x20\x20\x20\x2d\x2d\x3e\x0a"
+            ."\x3c\x21\x2d\x2d\x20\x20\x20\x6d\x61\x64\x63\x6f\x64\x65\x72\x40\x73\x74\x75\x64\x65\x6e\x74\x2e\x75\x74\x64\x61\x6c"
+            ."\x6c\x61\x73\x2e\x65\x64\x75\x20\x20\x20\x2d\x2d\x3e\x0a\x3c\x21\x2d\x2d\x20\x68\x74\x74\x70\x3a\x2f\x2f\x77\x77\x77"
+            ."\x2e\x75\x74\x64\x61\x6c\x6c\x61\x73\x2e\x65\x64\x75\x2f\x7e\x6d\x61\x64\x63\x6f\x64\x65\x72\x20\x2d\x2d\x3e\x0a\x0a";
+
+        // print($this->_seed);
+    }
+
+    // Return the current error code
+    function getError() {
+        return $this->_errorcode;
+    }
+
+    function isup() {
+        /************************************
+         * int isup(char * ip, long port);
+         * Return values:
+         *   0 = No response - probably down
+         *   1 = HL Responded - Server is up
+         *  -1 = Error in socket
+         ************************************/
+
+        if($ret = $this->_sockState()) {
+            // echo $ret;
+            return -1;
+        } else {
+            if($ret & 2) {
+                return 0;
+            }
+            $myPing = $this->_ping();
+            if($myPing > 0) {
+                return $myPing;
+            } else {
+                return 0;
+            }
+        }
+
     }
 
 
+    function Address  (){ return $this->_arr[ 0]; }
+    function Hostname (){ return $this->_arr[ 1]; }
+    function Map      (){ return $this->_arr[ 2]; }
+    function ModName  (){ return $this->_arr[ 3]; }
+    function Active   (){ return $this->_arr[ 5]; }
+    function Max      (){ return $this->_arr[ 6]; }
+    /
+    /** Don't always need these, so don't call if not needed */
+    function Protocol(){ return $this->_arr[ 7]; }
+    function SvrType (){ return $this->_arr[ 8]; }
+    function SvrOS   (){ return $this->_arr[ 9]; }
+    function Pass    (){ return $this->_arr[10]; }
+    function IsMod   (){ return $this->_arr[11]; }
+    function SvrOnly (){ return $this->_arr[17]; }
+    function Custom  (){ return $this->_arr[18]; }
 }
+
+/**
+ * Start displaying the server information
+ */
+
+$gameServer = new serverStatus($serverIP, $serverPort);
+$gameServer->getDetails();
+$gameServer->getPlayers();
+
+$theMod = $gameServer->ModName();
+$theMap = $gameServer->Map();
+
