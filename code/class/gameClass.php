bc82096e7f2bac5b8669c54a15de8a438ff7cc29
KiloooNL <benweidenhofer@live.com>
2017-04-20 10:48:51 +0930

    Added gameClass.php

---
+++ code/class/gameClass.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * GAMESERVERWATCHER
+ * 	coded by Ben Weidenhofer
+ * Published under the open-source GNU GPLv3 licence.
+ *
+ * GitHub repo: https://github.com/KiloooNL/GameServerWatcher
+ *
+ * gameClass.php
+ *
+ * This PHP class file is main class for ALL game engines.
+ * Within it, eventually will be every supported game in an array
+ * with a short name (EG: "CS:S") and a long name (EG: "Counter-Strike: Source")
+ *
+ * So that we can go,
+ * serverBanner("IP", "Port");
+ *
+ * findGameEngine($IP, $Port) {
+ *      // find game engine by running query
+ *      return gameEngine;
+ * }
+ *
+ * findGameEngine("IP", "Port");
+ */
+class gameClass {
+
+}
